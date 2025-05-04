c4b679d38c7039a0a143b4f958e4389785b968f0
bc2024-4 <bzabrodskij20@gmail.com>
2025-05-04 21:54:24 +0300

    add admin table news(redactor, topic, newspaper) and add forms for reset and changing password

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import Redactor, Topic, Newspaper
+
+
+@admin.register(Redactor)
+class RedactorAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'years_of_experience', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name')
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'topic')
+    search_fields = ('title', 'topic__name')
+    list_filter = ('published_date', 'topic')
--- news/forms.py
+++ news/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
+
 
 
 class TopicSearchForm(forms.Form):
@@ -29,3 +28,35 @@ class NewspaperSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "search by title"}),
     )
+
+
+User = get_user_model()
+
+
+class EmailVerificationForm(forms.Form):
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'placeholder': 'Ваша електронна адреса'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Користувача з таким email не знайдено.")
+        return email
+
+
+class SetNewPasswordForm(forms.Form):
+    new_password1 = forms.CharField(
+        label="Новий пароль",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Введіть новий пароль'}),
+    )
+    new_password2 = forms.CharField(
+        label="Підтвердіть пароль",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Повторіть новий пароль'}),
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("new_password1")
+        password2 = cleaned_data.get("new_password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Паролі не співпадають.")
+        return cleaned_data
